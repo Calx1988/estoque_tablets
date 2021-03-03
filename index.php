0bbbcfea70cbdd921dbf8d71649613d6d095cc46
Cássio Alexsander <cassioalexsander@gmail.com>
2021-03-03 07:17:40 -0300

    Created style.css

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
     <head>
         <title>Estoque de Tablets</title>
-
+        <meta charset="utf-8">
     
     </head>
     <body>
-    <p>Teste de escrita</p>
+    <h1>Estoque de Tablets</h1>
+    <form
     
     
     
