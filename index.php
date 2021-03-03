bb387fd10739cf025c68308c9c399fe75ec25cb8
Cássio Alexsander <cassioalexsander@gmail.com>
2021-03-03 07:36:19 -0300

    Created form for search in index.php

--- index.php
+++ index.php
@@ -6,8 +6,14 @@
     
     </head>
     <body>
-    <h1>Estoque de Tablets</h1>
-    <form
+        <h1>Estoque de Tablets</h1>
+        <form>
+            <input type:"text" name:"marca" placeholder:"Digite a marca...">
+            <input type:"text" name:"modelo" placeholder:"Digite a marca...">
+            <input type:"text" name:"cor" placeholder:"Digite a marca...">
+            <input type:"submit" name:"submit" value:"Buscar">
+        </form>
+
     
     
     
