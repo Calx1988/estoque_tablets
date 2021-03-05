a65ffc766fbc4be29e332ab9dcf8a7095ca24c1e
Cássio Alexsander <cassioalexsander@gmail.com>
2021-03-05 00:57:56 -0300

    created ul and css

--- index.php
+++ index.php
@@ -3,19 +3,25 @@
     <head>
         <title>Estoque de Tablets</title>
         <meta charset="utf-8">
+        <link rel= "stylesheet" href= "styles.css">
     
     </head>
     <body>
-        <h1>Estoque de Tablets</h1>
-        <form>
-            <input type:"text" name:"marca" placeholder:"Digite a marca...">
-            <input type:"text" name:"modelo" placeholder:"Digite a marca...">
-            <input type:"text" name:"cor" placeholder:"Digite a marca...">
-            <input type:"submit" name:"submit" value:"Buscar">
-        </form>
+        <ul>
+            <li><a href="#tablet">Tablet</a></li>
+                <ul>
+                    <li><a href="#buscar_tablet">Buscar Tablet</a></li>
+                    <li><a href="#cadastrar_tablet">Cadastrar Tablet</a></li>
+                </ul>
+            <li><a href="#fornecedor">Fornecedor</a></li>
+            <ul>
+                <li><a href="#buscar_fornecedor">Buscar Fornecedor</a></li>
+                <li><a href="#cadastrar_fornecedor">Cadastrar Fornecedor</a></li>
+            </ul>
+            <li><a href="#carregar_banco">Carregar Banco de Dados></a></li>
+            <li><a href="#sair">Sair</a></li>
+        </ul>
 
-    
-    
     
     </body>
 </html>
