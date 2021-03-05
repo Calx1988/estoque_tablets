2c6c9886e71710a84553d8b5028cce7975f372bc
Cássio Alexsander <cassioalexsander@gmail.com>
2021-03-05 07:20:36 -0300

    corrigidos alguns erros. criadas ul e list

--- index.php
+++ index.php
@@ -3,25 +3,27 @@
     <head>
         <title>Estoque de Tablets</title>
         <meta charset="utf-8">
-        <link rel= "stylesheet" href= "styles.css">
+        <link rel= "stylesheet" href= "styles/styles.css"/>
     
     </head>
     <body>
-        <ul>
-            <li><a href="#tablet">Tablet</a></li>
-                <ul>
-                    <li><a href="#buscar_tablet">Buscar Tablet</a></li>
-                    <li><a href="#cadastrar_tablet">Cadastrar Tablet</a></li>
-                </ul>
-            <li><a href="#fornecedor">Fornecedor</a></li>
+        <header class="topo">Controle de Estoque de Tablets</header>
+        <nav class="menu">
             <ul>
-                <li><a href="#buscar_fornecedor">Buscar Fornecedor</a></li>
-                <li><a href="#cadastrar_fornecedor">Cadastrar Fornecedor</a></li>
+                <li><a href="#">Tablets</a>
+                    <ul>
+                        <li><a href="#">Buscar Tablet</a></li>
+                        <li><a href="#">Cadastrar Tablet</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Fornecedores</a>
+                    <ul>
+                        <li><a href="#">Buscar Fornecedor</a></li>
+                        <li><a href="#">Cadastrar Fornecedor</a></li>
+                    </ul>
+                </li>
             </ul>
-            <li><a href="#carregar_banco">Carregar Banco de Dados></a></li>
-            <li><a href="#sair">Sair</a></li>
-        </ul>
+        </nav>
 
-    
     </body>
 </html>
