00a64fff009518d139c82ed15658b92e85949b00
Cássio Alexsander <cassioalexsander@gmail.com>
2021-03-06 23:08:28 -0300

    criando página buscarTablet.php. criando forms.

---
+++ buscarTablet.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Buscar Tablet</title>
+        <meta charset="utf-8">
+        <link rel= "stylesheet" href= "styles/styles.css"/>
+    </head>
+
+    <body>
+        <header class="topo">Controle de Estoque de Tablets</header>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">Tablets</a>
+                    <ul>
+                        <li><a href="#">Buscar Tablet</a></li>
+                        <li><a href="#">Cadastrar Tablet</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Fornecedores</a>
+                    <ul>
+                        <li><a href="#">Buscar Fornecedor</a></li>
+                        <li><a href="#">Cadastrar Fornecedor</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div>
+            <section class="corpo">
+                <form method="post" action="#">
+                    <input type="text" name="">
+                </form>
+
+            </section>
+        </div>
+    </body>
+    </html>
